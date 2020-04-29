deb7a6aba622cba6b5c2b015bb64d347eaea1a63
Rafael <rafaelsantos134500@gmail.com>
2020-04-28 21:40:35 -0300

    Nova Att

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDujCI6Wqy5pgZh_rCJZ0SA2zZMnzlBy-s',
-    authDomain: 'mygamesangular.firebaseapp.com',
-    databaseURL: 'https://mygamesangular.firebaseio.com',
-    projectId: 'mygamesangular',
-    storageBucket: 'mygamesangular.appspot.com',
-    messagingSenderId: '885848718029',
-    appId: '1:885848718029:web:6b698b97bf95b61d84c784'
+    apiKey: "AIzaSyApoL3MGn_j-xrl6DLnDizGC6_IcBMbV6o",
+    authDomain: "mygames1-a3b79.firebaseapp.com",
+    databaseURL: "https://mygames1-a3b79.firebaseio.com",
+    projectId: "mygames1-a3b79",
+    storageBucket: "mygames1-a3b79.appspot.com",
+    messagingSenderId: "83271597831",
+    appId: "1:83271597831:web:1f1c53846c1f5695de0fa2"
   }
 };
 
